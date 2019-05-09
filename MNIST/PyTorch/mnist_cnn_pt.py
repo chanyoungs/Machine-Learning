17e08096a14f3f1c21ffe16182bb35202ce0ca25
chanyoungs <chanyoungs@gmail.com>
2019-05-09 22:23:39 +0100

    pt_cnn_mnist

---
+++ MNIST/PyTorch/mnist_cnn_pt.py
@@ -0,0 +1,119 @@
+import numpy as np # to handle matrix and data operation
+import pandas as pd # to read csv and handle dataframe
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
+from torch.autograd import Variable
+
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('./train.csv')
+print(df.shape)
+
+y = df['label'].values
+X = df.drop(['label'],1).values
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
+
+print(y_test.shape)
+
+BATCH_SIZE = 32
+
+torch_X_train = torch.from_numpy(X_train).type(torch.LongTensor)
+torch_y_train = torch.from_numpy(y_train).type(torch.LongTensor) # data type is long
+
+# create feature and targets tensor for test set.
+torch_X_test = torch.from_numpy(X_test).type(torch.LongTensor)
+torch_y_test = torch.from_numpy(y_test).type(torch.LongTensor) # data type is long
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(torch_X_train,torch_y_train)
+test = torch.utils.data.TensorDataset(torch_X_test,torch_y_test)
+
+# data loader
+train_loader = torch.utils.data.DataLoader(train, batch_size = BATCH_SIZE, shuffle = False)
+test_loader = torch.utils.data.DataLoader(test, batch_size = BATCH_SIZE, shuffle = False)
+
+torch_X_train = torch_X_train.view(-1, 1,28,28).float()
+torch_X_test = torch_X_test.view(-1,1,28,28).float()
+print(torch_X_train.shape)
+print(torch_X_test.shape)
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(torch_X_train,torch_y_train)
+test = torch.utils.data.TensorDataset(torch_X_test,torch_y_test)
+
+# data loader
+train_loader = torch.utils.data.DataLoader(train, batch_size = BATCH_SIZE, shuffle = False)
+test_loader = torch.utils.data.DataLoader(test, batch_size = BATCH_SIZE, shuffle = False)
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
+        self.conv3 = nn.Conv2d(32,64, kernel_size=5)
+        self.fc1 = nn.Linear(3*3*64, 256)
+        self.fc2 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        #x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu(F.max_pool2d(self.conv3(x),2))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = x.view(-1,3*3*64 )
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+def fit(model, train_loader):
+    optimizer = torch.optim.Adam(model.parameters())#,lr=0.001, betas=(0.9,0.999))
+    error = nn.CrossEntropyLoss()
+    EPOCHS = 5
+    model.train()
+    for epoch in range(EPOCHS):
+        correct = 0
+        for batch_idx, (X_batch, y_batch) in enumerate(train_loader):
+            var_X_batch = Variable(X_batch).float()
+            var_y_batch = Variable(y_batch)
+            optimizer.zero_grad()
+            output = model(var_X_batch)
+            loss = error(output, var_y_batch)
+            loss.backward()
+            optimizer.step()
+
+            # Total correct predictions
+            predicted = torch.max(output.data, 1)[1] 
+            correct += (predicted == var_y_batch).sum()
+            #print(correct)
+            if batch_idx % 50 == 0:
+                print('Epoch : {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t Accuracy:{:.3f}%'.format(
+                    epoch, batch_idx*len(X_batch), len(train_loader.dataset), 100.*batch_idx / len(train_loader), loss.data.item(), float(correct*100) / float(BATCH_SIZE*(batch_idx+1))))
+
+def evaluate(model):
+#model = mlp
+    correct = 0 
+    for test_imgs, test_labels in test_loader:
+        #print(test_imgs.shape)
+        test_imgs = Variable(test_imgs).float()
+        output = model(test_imgs)
+        predicted = torch.max(output,1)[1]
+        correct += (predicted == test_labels).sum()
+    print("Test accuracy:{:.3f}% ".format( float(correct) / (len(test_loader)*BATCH_SIZE)))
+ 
+cnn = CNN()
+print(cnn)
+
+it = iter(train_loader)
+X_batch, y_batch = next(it)
+print(cnn.forward(X_batch).shape)
+
+fit(cnn,train_loader)
+
+evaluate(cnn)
